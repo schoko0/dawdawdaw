0879caddad22322873f9230961234593026811a4
schoko0 <svenniendorf@gmail.com>
2016-07-22 14:25:24 +0200

    summenrechnung

--- src/firstproject.java
+++ src/firstproject.java
@@ -2,8 +2,17 @@
  * Created by sniendorf on 22.07.2016.
  */
 public class firstproject {
-    int a = 1;
-    int b = 2;
-    int c = 3;
 
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        summe(a,b,c);
+    }
+
+    public int summe(int a, int b, int c){
+        int summe = a + b + c;
+        return summe
+    }
 }
