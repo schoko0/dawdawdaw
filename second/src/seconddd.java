c961cd54a995c8858c5e3954c7f34c625c460de5
schoko0 <svenniendorf@gmail.com>
2016-07-25 14:28:39 +0200

    mock test

--- second/src/seconddd.java
+++ second/src/seconddd.java
@@ -2,7 +2,7 @@
 public class seconddd {
 
 
-        public static void main(String[] args) {
+        public void main(String[] args) {
             int a = 1;
             int b = 2;
             int c = 3;
@@ -12,7 +12,7 @@ public class seconddd {
 
         }
 
-        private static int Summe(int a, int b, int c){
+        private int Summe(int a, int b, int c){
             int summea = a + b + c;
             int summeb = a + (int)Math.sqrt(b) - c;
             int result;
