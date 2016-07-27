8fa84021184a4493b77668d933d50aeb47c38e98
schoko0 <svenniendorf@gmail.com>
2016-07-27 14:44:25 +0200

    blubb

---
+++ Wiederholung GDP/src/InterfaceExercise/Dollar.java
@@ -0,0 +1,14 @@
+package InterfaceExercise;
+import static java.lang.Math.abs;
+import static java.lang.Math.signum;
+
+/**
+ * Created by sniendorf on 27.07.2016.
+ */
+public class Dollar extends Number {
+
+
+    public Dollar(int d, int c){
+
+    }
+}
