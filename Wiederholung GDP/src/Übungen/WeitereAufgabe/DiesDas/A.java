c029797f1fc95673d9804882c694baa251a1f155
schoko0 <svenniendorf@gmail.com>
2016-07-29 13:02:06 +0200

    Mittagspause

---
+++ Wiederholung GDP/src/Übungen/WeitereAufgabe/DiesDas/A.java
@@ -0,0 +1,40 @@
+package Übungen.WeitereAufgabe.DiesDas;
+import java.util.*;
+import java.awt.Point;
+/**
+ * Created by sniendorf on 29.07.2016.
+ */
+public class A {
+    public static void main(String[] args) {
+
+        List<Point> polygon = new ArrayList<>();
+        polygon.add(new Point(1, 1));
+        polygon.add(new Point(1, 2));
+        polygon.add(new Point(2, 1));
+        polygon.add(new Point(2, 2));
+        polygon.add(new Point(2, 1));
+        polygon.forEach(m -> System.out.format("(%d , %d)\n", m.x, m.y));
+
+        Random rnd = new Random();
+        Set<Integer> lotto = new HashSet<>();
+        do {
+            lotto.add(rnd.nextInt(49));
+        }while(lotto.size() < 6);
+        System.out.println( Arrays.toString(lotto.toArray()));
+
+        Map<String, String > dict = new HashMap<>();
+        dict.put("one", "eins");
+        dict.put("two", "zwei");
+        dict.put("three", "drei");
+        dict.put("four", "vier");
+        dict.put("five", "fünf");
+        dict.put("Nur der", "BVB");
+        System.out.println(dict.get("three"));
+        System.out.println(dict.get("Nur der"));
+    }
+
+
+
+}
+
+
