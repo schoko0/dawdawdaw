04112e1bbe507da6f9f6212b1dba0adac588ad04
schoko0 <svenniendorf@gmail.com>
2016-07-29 16:20:43 +0200

    j

---
+++ Wiederholung GDP/src/Übungen/WeitereAufgabe/DiesDas/SucheAusArrayDasGrößteElement.java
@@ -0,0 +1,25 @@
+package Übungen.WeitereAufgabe.DiesDas;
+
+/**
+ * Created by sniendorf on 29.07.2016.
+ */
+public class SucheAusArrayDasGrößteElement {
+
+    public static void main(String[] args) {
+
+        int a [] = {25,1,25,48,46,21,0,23,54,9,3,1,4,56,2,4,77,1,1,1,1,1,1,1,1,5,1};
+        blubb.sucheNachGrößterZahl(a);
+    }
+
+//    public static void sucheNachGrößterZahl(int [] a) {
+//
+//        int größteZahl = 0;
+//        for (int i = 0; i < a.length; i++) {
+//            if (größteZahl <= a[i]) {
+//                größteZahl = a[i];
+//            }
+//            W.w(größteZahl);
+//        }
+//    }
+ }
+
---
+++ Wiederholung GDP/src/Übungen/WeitereAufgabe/DiesDas/blubb.java
@@ -0,0 +1,21 @@
+package Übungen.WeitereAufgabe.DiesDas;
+
+/**
+ * Created by sniendorf on 29.07.2016.
+ */
+public class blubb {
+
+
+    public static void sucheNachGrößterZahl(int [] a) {
+
+        int größteZahl = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (größteZahl <= a[i]) {
+                größteZahl = a[i];
+            }
+
+        }
+        W.w(größteZahl);
+    }
+}
+
